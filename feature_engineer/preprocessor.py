d1d530dc8acb2c09cff505b077c8bdabdecbad7b
Daniel-Milanes <danielmilanesperez@gmail.com>
2025-06-25 13:45:30 -0600

    changing preprocessor

--- feature_engineer/preprocessor.py
+++ feature_engineer/preprocessor.py
@@ -1,13 +1,10 @@
 import pandas as pd
 from feature_engineer import (
-    VandalismScorer,
     is_IP,
     account_age,
     comment_empty,
     word_count,
 )
-from sklearn.model_selection import StratifiedKFold
-import numpy as np
 
 
 def preprocessor(df: pd.DataFrame) -> None:
@@ -33,19 +30,3 @@ def preprocessor(df: pd.DataFrame) -> None:
     df["word_count_added"], df["word_count_deleted"] = zip(
         *df.apply(word_count, axis=1)
     )
-
-    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
-    df["vandalism_score"] = np.zeros(df.shape[0], dtype=float)
-
-    for train_index, test_index in cv.split(df, df["isvandalism"]):
-        train_df = df.iloc[train_index]
-        test_df = df.iloc[test_index]
-
-        scorer = VandalismScorer()
-        scorer.fit(
-            train_df["added_lines"], train_df["deleted_lines"], train_df["isvandalism"]
-        )
-        test_scores = scorer.score(test_df["added_lines"], test_df["deleted_lines"])
-
-        df.loc[test_df.index, "vandalism_score"] = test_scores
